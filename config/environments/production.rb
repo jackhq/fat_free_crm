be14caf85cfec813c4b49c7025810e6b147ab10e
Tom Wilson <tom@jackhq.com>
2010-08-10 23:22:08 -0400

    added sendmail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,14 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
-
+config.action_mailer.smtp_settings = {
+  :tls => true,
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'jackrussellsoftware.com',
+  :authentication => :plain,
+  :user_name => 'do-not-reply@jackrussellsoftware.com',
+  :password => 'jackruss'
+}
 # Enable threaded mode
 # config.threadsafe!
